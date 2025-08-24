dfecde8f029eae7c357fef59f5f6e995bbcd0a98
choazephyr <proudinlove@gmail.com>
2025-08-24 14:59:15 +0700

    Update like_workspace
    edit import error

--- main-fiber/like_workspace/internal/routes/routes.go
+++ main-fiber/like_workspace/internal/routes/routes.go
@@ -4,11 +4,11 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 
-	"like_workspace/internal/handlers"
+	"github.com/pllus/main-fiber/like_workspace/internal/handlers"
 )
 
 func RegisterRoutes(app *fiber.App, client *mongo.Client) {
-	app.Get("/hello", handlers.GetHello)
+	//app.Get("/hello", handlers.GetHello)
 	app.Get("/Post", func(c *fiber.Ctx) error {
 		return handlers.GetUserHandler(c, client)
 	})
